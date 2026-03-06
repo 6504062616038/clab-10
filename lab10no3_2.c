b0c67b8cbf0f580026647f6e50d88337f9ea3e70
6504062616038 <153298618+6504062616038@users.noreply.github.com>
2026-03-07 01:12:20 +0700

    Add files via upload

---
+++ lab10no3_2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct Books{
+    int BookID;
+    char BookTitle[50];
+};
+
+void DisplayData(struct Books *CBook);
+
+int main() {
+
+    struct Books ComputerBook;
+
+    printf("Enter Book ID :");
+    scanf("%d",&ComputerBook.BookID);
+
+    printf("Enter Book Title :");
+    scanf("%s",ComputerBook.BookTitle);
+
+    DisplayData(&ComputerBook);
+
+    return 0;
+}
+
+void DisplayData(struct Books *CBook){
+    printf("Book ID : %d\n",CBook->BookID);
+    printf("Book Title : %s\n",CBook->BookTitle);
+}
